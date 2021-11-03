b5096dd506b90efc99dc71d59a786f4cd9f5c334
AnshVM <anshmalik2002@gmail.com>
2021-11-04 00:47:46 +0530

    Basic project setup

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const app = express();
+
+dotenv.config();
+
+mongoose
+    .connect(process.env.DB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      })
+    .then(()=>console.log("DB connected"))
+    .catch(()=>console.log("Cannot connect to DB"))
+
+app.use(express.json());
+
+const port = process.env.PORT;
+app.listen(port,()=>{
+    console.log(`Server started on port ${port}`);
+})
